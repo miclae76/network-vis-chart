164d6f4325b4cde95f7c9d63892aa039d30c14c3
Li Kang <kkllpaul7766@gmail.com>
2022-07-29 20:04:18 +0200

    fix: eslint error (#30)

--- src/_test/tooltip.jest.test.js
+++ src/_test/tooltip.jest.test.js
@@ -1,5 +1,11 @@
-import { createTooltipHTML } from '../tooltip';
+import { createTooltipHTML } from "../tooltip";
 
-test("Testing createTooltipHtml function - Name- GroupNumber- nodeMeasure", () => {
-    expect(createTooltipHTML({ name: "Venice", groupNumber: 1, nodeMeasure: 2 })).toContainHTML("<div><div><span>Name: </span><b>Venice</b></div><div><span>Group number: </span><b>1</b></div><div><span>Node measure: </span><b>2</b></div></div>");
+describe("createTooltipHTML", () => {
+  it("Testing createTooltipHtml function - Name- GroupNumber- nodeMeasure", () => {
+    expect(
+      createTooltipHTML({ name: "Venice", groupNumber: 1, nodeMeasure: 2 })
+    ).toContainHTML(
+      "<div><div><span>Name: </span><b>Venice</b></div><div><span>Group number: </span><b>1</b></div><div><span>Node measure: </span><b>2</b></div></div>"
+    );
+  });
 });
