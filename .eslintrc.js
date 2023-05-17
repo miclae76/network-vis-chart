af798708c4d5b33c7dc10ba4cf791997477d784a
Tobias Åström <tsm@qlik.com>
2023-05-17 10:11:41 +0200

    chore: update dependencies (#42)
    * chore: update nebula
    
    * chore: all the deps
    
    * chore: set node

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
     ecmaVersion: 6,
     ecmaFeatures: {
       jsx: true,
-      modules: true
+      modules: true,
     },
-    sourceType: "module"
+    sourceType: "module",
   },
-  parser: "babel-eslint",
+  parser: "@babel/eslint-parser",
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   globals: {
     $: false,
@@ -21,41 +21,48 @@ module.exports = {
     document: false,
     expect: false,
     it: false,
-    require: false
+    require: false,
   },
   rules: {
-    "indent": ["error", 2, { "SwitchCase": 1 }],
+    indent: ["error", 2, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
     "object-curly-spacing": ["error", "always"],
     "max-lines": ["warn", 300],
     "max-len": ["warn", 120],
     "no-console": ["warn"],
-    "no-mixed-operators": ["warn", {
-      "groups": [
-        ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
-        ["&&", "||"],
-        ["in", "instanceof"]
-      ],
-      "allowSamePrecedence": true
-    }],
+    "no-mixed-operators": [
+      "warn",
+      {
+        groups: [
+          ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+          ["&&", "||"],
+          ["in", "instanceof"],
+        ],
+        allowSamePrecedence: true,
+      },
+    ],
     "no-multi-spaces": ["error"],
     "no-cond-assign": ["warn"],
     "no-fallthrough": ["warn"],
     "no-undef": ["warn"],
     "no-unused-vars": ["error"],
-    "no-use-before-define": ["warn", { "functions": false, "classes": false, "variables": false }],
+    "no-use-before-define": [
+      "warn",
+      { functions: false, classes: false, variables: false },
+    ],
     "no-useless-escape": ["warn"],
     "no-useless-return": ["warn"],
-    "no-underscore-dangle": ["warn", { "allow": ["_id"] }],
+    "no-underscore-dangle": ["warn", { allow: ["_id"] }],
     "no-redeclare": ["warn"],
     "no-restricted-syntax": ["warn"],
     "operator-linebreak": ["warn", "before"],
     "prefer-promise-reject-errors": ["warn"],
-    "padded-blocks": ["warn", { "blocks": "never", "switches": "never", "classes": "never" }],
-    "semi": ["error", "always"],
-    "valid-typeof": ["warn"]
+    "padded-blocks": [
+      "warn",
+      { blocks: "never", switches: "never", classes: "never" },
+    ],
+    semi: ["error", "always"],
+    "valid-typeof": ["warn"],
   },
-  extends: [
-    "eslint:recommended"
-  ]
-}
+  extends: ["eslint:recommended"],
+};
